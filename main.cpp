b0a49fdd6a1261ad0b96016bd2c97c46f5c71b98
juan esteban <esteban_rlt159@hotmail.com>
2021-11-26 14:54:23 -0500

    agregamos commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char l = 65;
+    for(int i=0;i<=6;i++)
+    {
+        l = 65;
+        for(int j =0;j<=i;j++)
+            {
+            cout <<l;
+            l++;
+            }
+            l--;
+        if(i!=6)
+            {
+            for(int k=0;k<=10-2*i;k++)
+            cout<<" ";
+            }
+        if(i==6)
+            l--;
+        for(int w=0;w<=i;w++)
+            {
+            cout<<l;
+            l--;
+            if(i==6 and w==5)
+                w++;
+        }
+        cout<<endl;
+
+    }
+    for(int i=5;i>=0;i--)
+        {
+        l = 65;
+        for(int j=0;j<=i;j++)
+            {
+            cout<<l;
+            l++;
+            }
+        l--;
+        for(int k=0;k<=10-2*i;k++)
+            cout<<" ";
+        for(int w=0;w<=i;w++)
+            {
+            cout<<l;
+            l--;
+            }
+        cout<<endl;
+        }
+    return 0;
+}
